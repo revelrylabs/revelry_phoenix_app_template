ab82ee659b83f090271c11f021556a1de745e141
Brian Berlin <bri@nberl.in>
2019-02-05 08:10:33 -0600

    Updates to Nav and routes for auth/unauthed users

--- lib/app_template_web/plugs/require_auth.ex
+++ lib/app_template_web/plugs/require_auth.ex
@@ -10,19 +10,15 @@ defmodule AppTemplateWeb.RequireAuth do
     opts
   end
 
-  def call(conn, _) do
-    case conn.assigns.current_user do
-      nil ->
-        path = "#{conn.request_path}?#{conn.query_string}"
-        encoded_path = URI.encode_www_form(path)
-        to_path = Routes.session_path(conn, :new, next: encoded_path)
+  def call(%Plug.Conn{assigns: %{current_user: nil}} = conn, _) do
+    path = "#{conn.request_path}?#{conn.query_string}"
+    encoded_path = URI.encode_www_form(path)
+    to_path = Routes.session_path(conn, :new, next: encoded_path)
 
-        conn
-        |> Phoenix.Controller.redirect(to: to_path)
-        |> halt()
-
-      _user ->
-        conn
-    end
+    conn
+    |> Phoenix.Controller.redirect(to: to_path)
+    |> halt()
   end
+
+  def call(conn, _), do: conn
 end
