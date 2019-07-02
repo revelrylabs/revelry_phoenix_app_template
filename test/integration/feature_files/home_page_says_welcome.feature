cb048a81d95ece142ddd3814cea2fbc27b6b5dc3
maryolivier <maryolegendre@gmail.com>
2019-07-02 15:00:46 -0500

    adds cabbage feature file and feature test

---
+++ test/integration/feature_files/home_page_says_welcome.feature
@@ -0,0 +1,3 @@
+Scenario: User on the home page sees a Welcome message
+When I navigate to the home screen
+Then I see a welcome message
---
+++ test/integration/feature_tests/home_page_says_welcome_test.exs
@@ -0,0 +1,15 @@
+defmodule AppTemplateWeb.HomePageSaysWelcomeTest do
+  use Cabbage.Feature,
+    async: true,
+    file: "home_page_says_welcome.feature"
+  use AppTemplateWeb.IntegrationCase
+
+  defwhen ~r/^I navigate to the home screen$/, _variables, _state do
+    navigate_to("/")
+  end
+
+  defthen ~r/^I see a welcome message$/, _variables, _state do
+    element = find_element(:tag, "h1")
+    assert inner_html(element) == "Welcome"
+  end
+end
