61de9b697ed1e75a48e1304725e8689fe373ac90
Bryan Joseph <bryanjos@users.noreply.github.com>
2019-12-11 09:43:36 -0600

    Debug migration

--- lib/release_tasks.ex
+++ lib/release_tasks.ex
@@ -71,7 +71,7 @@ defmodule AppTemplate.ReleaseTasks do
   defp run_migrations_for(repo) do
     app = Keyword.get(repo.config, :otp_app)
     IO.puts("Running migrations for #{app}")
-    IO.puts("Using #{repo.config |> Keyword.drop([:password])}")
+    IO.puts("Using #{repo.config |> Keyword.drop([:password]) |> inspect()}")
     migrations_path = priv_path_for(repo, "migrations")
     Ecto.Migrator.run(repo, migrations_path, :up, all: true)
   end
