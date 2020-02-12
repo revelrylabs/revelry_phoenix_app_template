ed902b55bd7f4c67018053c3127fa0c5c65d7d2d
Hung Tran <hung@0x0.ninja>
2020-02-12 08:44:54 -0600

    Make Clustering Optional (#510)
    * add option to disable cluster logic in release
    
    * remove unused statix config

--- config/releases.exs
+++ config/releases.exs
@@ -20,11 +20,6 @@ config :rollbax,
   # TODO: turn on when your app is deployed
   enabled: false
 
-config :app_template, :statix,
-  prefix: "app_template",
-  host: System.get_env("DATADOG_HOST") || "100.66.67.91",
-  port: String.to_integer(System.get_env("DATADOG_PORT") || "8125")
-
 config :app_template, AppTemplate.Mailer,
   adapter: Bamboo.SendGridAdapter,
   api_key: System.get_env("SENDGRID_API_KEY")
@@ -32,7 +27,10 @@ config :app_template, AppTemplate.Mailer,
 config :app_template,
   jwt_secret: System.get_env("JWT_SECRET") || System.get_env("SECRET_KEY_BASE")
 
+# Configure Cluster Nodes
 app_name = System.get_env("APP_NAME") || "app-template"
+
+config :app_template, cluster_disabled: System.get_env("CLUSTER_DISABLED") == "1"
 
 config :app_template,
   cluster_topologies: [
