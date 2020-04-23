76efbfa099ad6ef6a91e4de1dfd7025582256d62
Bryan Joseph <bryanjos@users.noreply.github.com>
2020-04-22 19:30:29 -0500

    fix test

--- assets/js/app.js
+++ assets/js/app.js
@@ -26,20 +26,21 @@ function updateTimeTags() {
 }
 
 function setupLiveView() {
-  let csrfToken = document
-    .querySelector("meta[name='csrf-token']")
-    .getAttribute('content')
-  let liveSocket = new LiveSocket('/live', Socket, {
-    params: {_csrf_token: csrfToken},
-  })
+  const csrfTokenHeader = document.querySelector("meta[name='csrf-token']")
+  if (csrfTokenHeader) {
+    const csrfToken = csrfTokenHeader.getAttribute('content')
+    const liveSocket = new LiveSocket('/live', Socket, {
+      params: {_csrf_token: csrfToken},
+    })
 
-  // connect if there are any LiveViews on the page
-  liveSocket.connect()
+    // connect if there are any LiveViews on the page
+    liveSocket.connect()
 
-  // expose liveSocket on window for web console debug logs and latency simulation:
-  // >> liveSocket.enableDebug()
-  // >> liveSocket.enableLatencySim(1000)
-  window.liveSocket = liveSocket
+    // expose liveSocket on window for web console debug logs and latency simulation:
+    // >> liveSocket.enableDebug()
+    // >> liveSocket.enableLatencySim(1000)
+    window.liveSocket = liveSocket
+  }
 }
 
 function init(_config) {
