c3b761dcfe430dd1cbba52bd4b78063f37014f2a
ijadams <ijadams503@gmail.com>
2021-05-17 12:27:42 -0400

    add esm to for es6 export on mocha tests

---
+++ assets/test/app_test.js
@@ -0,0 +1,28 @@
+const expect = require('chai').expect
+const {JSDOM} = require('jsdom')
+
+import App from '../js/app'
+
+describe('App', () => {
+    describe('init', () => {
+        it('updates times', () => {
+            const timeTagHTML = `
+      <time datetime="2018-12-06T22:56:17Z" title="2018-12-06T22:56:17Z">2018-12-06T22:56:17Z</time>
+      `
+            const jsdom = new JSDOM(timeTagHTML)
+
+            const {window} = jsdom
+            const {document} = window
+
+            global.window = window
+            global.document = document
+
+            App.init()
+
+            const timeTagElement = document.querySelector('time[datetime]')
+
+            expect(timeTagElement.textContent).to.have.string('December')
+        })
+    })
+})
+
